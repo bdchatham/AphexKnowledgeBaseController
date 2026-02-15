2cd173dfbfb362399a358e858473fbb1451935e3
bdchatham <bdchatham@gmail.com>
2026-02-15 13:56:06 -0800

    Register tekton pipeline v1 scheme for TaskRun creation

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	triggersv1beta1 "github.com/tektoncd/triggers/pkg/apis/triggers/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -34,6 +35,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(platformv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(tektonv1.AddToScheme(scheme))
 	utilruntime.Must(triggersv1beta1.AddToScheme(scheme))
 }
 
